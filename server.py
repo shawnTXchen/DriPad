1a239ec94fe16c17cca9fb8d1a6b3af53b02a73d
Tianxiang Shawn Chen <Shawn@SeanMac.local>
2016-03-28 20:38:20 -0400

    ver1
    pass instructions to server and extract direction

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import time
 import BaseHTTPServer
+import urllib
 
 
 HOST_NAME = '127.0.0.1' # !!!REMEMBER TO CHANGE THIS!!!
@@ -16,30 +17,27 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         BaseHTTPServer.BaseHTTPRequestHandler.end_headers(self)
     def do_GET(s):
         from urlparse import urlparse, parse_qs
-        
-        # print "---Path name: "
-        # print s.path
 
         query_components = parse_qs(urlparse(s.path).query)
-        
-        # print "---Direction: "
-        # print query_components["direction"][0]
-        
-        direction = query_components["direction"] 
-        # name(direction[0])
-        print "---direction:"
-        print direction[0]
+        instrucion = urllib.unquote(query_components["instrucion"][0])
+
+        print '---instrucion:'
+        print instrucion
+
+        if 'left' in instrucion:
+            print '<== Turn left'
+        if 'right' in instrucion:
+            print '==> Turn right'
 
         """Respond to a GET request."""
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
+
         # s.wfile.write("<html><head><title>Title goes here.</title></head>")
         # s.wfile.write("<body><p>This is a page."+ direction[0] +"</p>")
-
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
-
         # s.wfile.write("<p>You accessed path: %s</p>" % s.path)
         # s.wfile.write("</body></html>")
 
